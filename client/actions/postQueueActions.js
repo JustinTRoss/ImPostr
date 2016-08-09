145593612ec1aa206c59b7a2da3937f3fd58c78a
Matt Dubie <mbdubie@gmail.com>
2016-08-09 13:15:41 -0700

    post queue actions are now covered by tests

--- client/actions/postQueueActions.js
+++ client/actions/postQueueActions.js
@@ -1,14 +1,6 @@
 export const REMOVE_ITEM_FROM_QUEUE = 'REMOVE_ITEM_FROM_QUEUE';
 export const INSERT_ITEM_FROM_QUEUE = 'INSERT_ITEM_FROM_QUEUE';
 
-// presumably, we're inserting or removing an item from post queue here
-// we'd also need to pass an index because simple
-// pop/push doesn't encompass inserting in the middle
-
-//moves to the bottom of the list
-//changes stlying
-//gets removed once the time expires by worker 
-
 //allows user to veto and item in the queue
 export const removeItem = (index) => ({
   type: REMOVE_ITEM_FROM_QUEUE,
