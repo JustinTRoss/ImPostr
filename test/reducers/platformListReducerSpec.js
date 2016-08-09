fb5faaa5b2bbd1182a4bfa7cc5d45e0512790240
Matt Dubie <mbdubie@gmail.com>
2016-08-09 15:34:05 -0700

    code testing coverage for post queue reducer

---
+++ test/reducers/platformListReducerSpec.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+
+import reducer from '../../client/reducers/platformList';
+
+import {
+  LOGIN_PLATFORM,
+  LOGOUT_PLATFORM,
+  TOGGLE_MODAL,
+  RECEIVE_SETTINGS_FIELDS,
+} from '../../client/actions/platformListActions';
+
+describe('Platform List Reducer', () => {
+
+  it('should return initial state');
+
+  it('should handle LOGIN_PLATFORM');
+
+  it('should handle LOGOUT_PLATFORM');
+
+  it('should handle TOGGLE_MODAL');
+
+  it('should handle RECEIVE_SETTINGS_FIELDS');
+});
