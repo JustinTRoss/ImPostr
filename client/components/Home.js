97a15c5ea850d8444bf2974ee72341577d29b089
Matt Dubie <mbdubie@gmail.com>
2016-08-08 22:18:43 -0700

    user can enter form settings

--- client/components/Home.js
+++ client/components/Home.js
@@ -2,6 +2,11 @@ import React from 'react';
 import PlatformListContainer from '../containers/PlatformListContainer';
 import PostQueueContainer from '../containers/PostQueueContainer';
 
+<<<<<<< 0addbcfd3e060febcc830a4402240ef8a0560d49:client/components/Home.js
+=======
+import PlatformListContainer from '../containers/PlatformListContainer';
+import PostQueueContainer from '../containers/PostQueueContainer';
+>>>>>>> user can enter form settings:client/components/Home.js
 
 const Home = () => (
   <div>Home
