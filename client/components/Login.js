f59da5d1b9aefabcd9671ab11c5fa985460753d5
Justin Ross <Ross.Justin.T@gmail.com>
2016-08-09 00:11:32 -0700

    Allow users to sign in and sign out - actions still not found

--- client/components/Login.js
+++ client/components/Login.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-const Login = () => {
+const Login = ({ username, handleUsernameChange, password, handlePasswordChange, handleLoginSubmit }) => {
   return (
     <div>
-      Login
+      <input
+        name="username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <input
+        name="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button onClick={handleLoginSubmit}> Submit </button>
     </div>
   );
 }
-
-
 
 export default Login;
