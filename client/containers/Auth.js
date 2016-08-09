2d7dcd05640095ec7f8c13d989efc5cfbcae9d12
Justin Ross <Ross.Justin.T@gmail.com>
2016-08-08 20:19:08 -0700

    Implement broken auth

--- client/containers/Auth.js
+++ client/containers/Auth.js
@@ -8,6 +8,7 @@ import Signup from '../components/Signup';
 class Auth extends React.Component {
   constructor(props) {
     super(props);
+<<<<<<< 67239dfa5ccfb2c8554cf42aa6882dfb22a8381c
 
     this.handleSignupSubmit = this.props.handleSignupSubmit.bind(this);
     this.handleLoginSubmit = this.props.handleLoginSubmit.bind(this);
@@ -51,14 +52,44 @@ class Auth extends React.Component {
           console.log(this.handleSignupSubmit);
           this.handleSignupSubmit(this.state.signup);
         }}
+=======
+    this.handleLoginUsernameChange = this.handleLoginUsernameChange.bind(this)
+    this.handleLoginPasswordChange = this.handleLoginPasswordChange.bind(this);
+    this.handleSignupUsernameChange = this.handleSignupUsernameChange.bind(this);
+    this.handleSignupPasswordChange = this.handleSignupPasswordChange.bind(this);
+    this.handleSignupFullNameChange = this.handleSignupFullNameChange.bind(this);
+  }
+
+  render() {
+    const childToRender = this.props.isLogin ?
+      <Login
+        username={}
+        password={}
+        handleUsernameChange={handleLoginUsernameChange}
+        handlePasswordChange={handleLoginPasswordChange}
+      /> : <Signup
+        username={}
+        password={}
+        fullName={}
+        handleUsernameChange={handleSignupUsernameChange}
+        handlePasswordChange={handleSignupPasswordChange}
+        handlefullNameChange={handleSignupfullNameChange}
+>>>>>>> Implement broken auth
       />;
 
     return (
       <div>
+<<<<<<< 67239dfa5ccfb2c8554cf42aa6882dfb22a8381c
         <button onClick={() => this.props.handleFormChange('signup')}>
           Signup
         </button>
         <button onClick={() => this.props.handleFormChange('login')}>
+=======
+        <button onClick={isLogin = false}>
+          Signup
+        </button>
+        <button onClick={isLogin = true}>
+>>>>>>> Implement broken auth
           Login
         </button>
         {childToRender}
@@ -67,6 +98,7 @@ class Auth extends React.Component {
   }
 }
 
+<<<<<<< 67239dfa5ccfb2c8554cf42aa6882dfb22a8381c
 
 const mapStateToProps = state => {
   return {
@@ -77,6 +109,31 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
+=======
+function handleLoginUsernameChange(e) {
+
+}
+function handleLoginPasswordChange(e) {
+
+}
+function handleSignupUsernameChange(e) {
+
+}
+function handleSignupPasswordChange(e) {
+
+}
+function handleSignupfullNameChange(e) {
+
+}
+
+function mapStateToProps({loggedIn}) {
+  return {
+    loggedIn,
+    isLogin,
+  };
+}
+function mapDispatchToProps({loggedIn}) {
+>>>>>>> Implement broken auth
   return {
     handleFormChange: formType => {
       dispatch(changeFormType(formType));
--- client/containers/Home.js
+++ client/containers/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+<<<<<<< 67239dfa5ccfb2c8554cf42aa6882dfb22a8381c
 import PlatformListContainer from './PlatformListContainer';
 import PostQueueContainer from './PostQueueContainer';
 
@@ -15,3 +16,37 @@ const Home = () => (
 );
 
 export default Home;
+=======
+import PlatformList from '../components/PlatformList';
+import PostQueue from '../components/PostQueue';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Home
+        <div>
+          <PlatformList platforms={this.props.platformList} />
+        </div>
+        <div>
+          <PostQueue posts={this.props.postQueue} />
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    platformList: state.platformList,
+    postQueue: state.postQueue,
+  };
+}
+
+Home = connect(mapStateToProps)(Home);
+
+export default Home;
+>>>>>>> Implement broken auth
