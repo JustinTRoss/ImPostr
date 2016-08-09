b80bdb0a9ab2cc181f06431324952760dc272241
Justin Ross <Ross.Justin.T@gmail.com>
2016-08-09 15:01:22 -0700

    Complete auth functionality

--- server/config/initMiddleware.js
+++ server/config/initMiddleware.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = {
   init: (app) => {
     console.log(`morgan/body parser`);
     app.use(morgan('dev'));
     app.use(bodyParser.json());
+    app.use(cors());
   },
 };
