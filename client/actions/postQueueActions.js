a16c7ba6c4fccaa979c8b0dcd5efd1daf910f45a
Matt Dubie <mbdubie@gmail.com>
2016-08-09 15:34:05 -0700

    code testing coverage for post queue reducer

--- client/actions/postQueueActions.js
+++ client/actions/postQueueActions.js
@@ -1,13 +1,11 @@
 export const REMOVE_ITEM_FROM_QUEUE = 'REMOVE_ITEM_FROM_QUEUE';
 export const INSERT_ITEM_FROM_QUEUE = 'INSERT_ITEM_FROM_QUEUE';
 
-//allows user to veto and item in the queue
 export const removeItem = (index) => ({
   type: REMOVE_ITEM_FROM_QUEUE,
   index,
 });
 
-//allows user to put a removed item back into queue
 export const insertItem = (index) => ({
   type: INSERT_ITEM_FROM_QUEUE,
   index,
