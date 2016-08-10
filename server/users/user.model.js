e59a7917abab733979259a6bf04c005b5a848487
Steven Chung <steven.chung1@gmail.com>
2016-08-10 10:22:19 -0700

    Initial user model re-model and bcrypt util

--- server/users/user.model.js
+++ server/users/user.model.js
@@ -1,12 +1,9 @@
 const sequelize = require('../config/sequelize');
 const Sequelize = require('sequelize');
 
-
 const User = sequelize.define('user', {
-  username: { type: Sequelize.STRING, allowNull: false, unique: true},
+  username: { type: Sequelize.STRING, allowNull: false, unique: true },
   password: { type: Sequelize.STRING, allowNull: false },
-  fullName: {type: Sequelize.STRING, allowNull: false},
-  email: {type: Sequelize.STRING, allowNull: false, unique: true}
 });
 
 User.sync();
