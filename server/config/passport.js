56d425dfa0e09a7d638a933f8bdebf5191d467cd
Steven Chung <steven.chung1@gmail.com>
2016-08-12 14:58:37 -0700

    Attempt at using Passport-OAUTH2LinkedIn

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,6 +3,8 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../users/user.model');
 const config = require('./config');
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+const { LINKEDIN_KEY, LINKEDIN_SECRET } = require('../../__cutestuff');
 
 passport.use(new JwtStrategy(
   {
@@ -19,4 +21,14 @@ passport.use(new JwtStrategy(
     })
     .catch(done);
   }
+));
+
+passport.use(new LinkedInStrategy({
+  clientID: LINKEDIN_KEY,
+  clientSecret: LINKEDIN_SECRET,
+  callbackURL: `http://127.0.0.1:3000/auth/linkedin/callback`,
+  scope: ['r_emailaddress', 'r_basicprofile', 'w_share'],
+}, (...args) => {
+  console.log(args, 'what is passed in?');
+}
 ));
--- server/config/routes.js
+++ server/config/routes.js
@@ -2,11 +2,24 @@ const SettingsRouter = require('../settings/setting.routes');
 const PostRouter = require('../posts/post.routes');
 const UserRouter = require('../users/user.routes');
 const passport = require('passport');
+const secretState = require('./config');
+console.log(secretState);
 
 module.exports = (app, config) => {
   app.use('/settings', passport.authenticate('jwt', {session: false}), SettingsRouter);
   app.use('/post', passport.authenticate('jwt', {session: false}), PostRouter);
   app.use('/user', UserRouter);
+
+  // --- leave me alone for now (Steven)
+  app.get('/auth/linkedin',
+    passport.authenticate('linkedin', { state: 'oasidfjoaisdjf' }),
+    (req, res) => { /*will not be called since redirect to linkedIn*/}
+  );
+
+  app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  }));
 
   // app.get('/*', (req, res) => {
   //   res.sendFile(config.rootPath + '/client/index.html');
