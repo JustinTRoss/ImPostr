3d1c90797ecf0646494c381829093e63bde1480d
Steven Chung <steven.chung1@gmail.com>
2016-08-13 16:57:57 -0700

    LinkedIn Methods for posting user queue

--- server/platformservices/linkedin.js
+++ server/platformservices/linkedin.js
@@ -1,5 +1,31 @@
 const Setting = require('../settings/setting.model');
 
-const postToLinkedIn = () => {};
+const postToLinkedIn = ({ token, message }) => {
+  let messageObject = {
+    comment: 'Pillow Talk',
+    content: {
+      title: `Hack Reactor and You`,
+      description: `pillows`,
+      'submitted-url': message,
+      'submitted-image-url': 'https://example.com/logo.png',
+    },
+    visibility: {
+      code: 'anyone',
+    },
+  };
+  return fetch(`https://api.linkedin.com/v1/people/~/shares?oauth2_access_token=${token}&format=json`, {
+    method: 'POST',
+    body: JSON.stringify(messageObject),
+    headers: new Headers({
+      'Content-Type': 'application/json',
+      'x-li-format': 'json',
+    }),
+  })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
 
-module.exports = {};
+module.exports = {
+  postToLinkedIn,
+};
