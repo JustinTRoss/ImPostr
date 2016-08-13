02849505a1e9b683e6a46ef0e8fafc1f7e38f8c7
Matt Dubie <mbdubie@gmail.com>
2016-08-13 09:48:10 -0700

    to merge

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -3,6 +3,7 @@ const jwt = require('jwt-simple');
 const fetch = require('isomorphic-fetch');
 const config = require('../config/config');
 const { LINKEDIN_KEY, LINKEDIN_SECRET } = require('../../__cutestuff');
+const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = require('../../__cutestuff');
 
 const saveTwitterTokens = (req, res) => {
   const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
@@ -34,12 +35,14 @@ const saveTwitterTokens = (req, res) => {
 };
 
 const saveLinkedInToken = (req, res) => {
+console.log('saveLinkedInToken');
   const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
   fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${req.query.code}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Flinkedin%2Fcallback&client_id=${LINKEDIN_KEY}&client_secret=${LINKEDIN_SECRET}`, {
     method: 'POST',
-    headers: new Headers({
+    headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-    }),
+      'cors': 'no-cors',
+    },
   })
   .then(res => res.json())
   .then(json => {
@@ -69,7 +72,63 @@ const saveLinkedInToken = (req, res) => {
   .catch(err => console.log(err));
 };
 
+const saveFacebookToken = (req, res) => {
+  console.log('req gettting hit', req);
+  const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
+  console.log('userId', userId);
+  const url = `https://www.facebook.com/dialog/oauth?client_id=${FACEBOOK_APP_ID}&response_type=token&redirect_uri=http%3A%2F%2Fwww.localhost%3A3000%2Fauth%2Ffacebook%2Fcallback`
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'cors': 'no-cors',
+    },
+  })
+  .then(response => {
+  console.log('response', response);
+    res.redirect('/')
+  })
+
+  // .then(res => {
+  //   res.json()
+  // })
+  // .then(json => {
+  //   console.log(json, 'THIS IS OUR TOKEN!');
+  //   Settings.findOne({
+  //     where: {
+  //       userUserId: userId,
+  //       platform: 'facebook',
+  //     },
+  //   })
+  //   .then(settings => {
+  //     if (settings) {
+  //       settings.updateAttributes({
+  //         token: json.access_token,
+  //       });
+  //     } else {
+  //       Settings.create({
+  //         userUserId: userId,
+  //         platform: 'facebook',
+  //         token: json.access_Token,
+  //       });
+  //     }
+  //   });
+  // })
+  // .then(() => {
+  //   res.redirect('/');
+  // })
+  // .catch(err => console.log(err));
+
+};
+
+
+
+
 module.exports = {
   saveLinkedInToken,
+<<<<<<< 6da89c3e3937c5b6c5c0ecf686563e9dbbee7ab5:server/auth/auth.controller.js
   saveTwitterTokens,
+=======
+  saveFacebookToken,
+>>>>>>> to merge:server/auth/authcontroller.js
 };
--- server/config/passport.js
+++ server/config/passport.js
@@ -8,6 +8,8 @@ const config = require('./config');
 const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const { LINKEDIN_KEY, LINKEDIN_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET } = require('../../__cutestuff');
 
+
+
 passport.use(new JwtStrategy(
   {
     secretOrKey: config.secret,
@@ -36,6 +38,7 @@ passport.use(new LinkedInStrategy({
 }
 ));
 
+<<<<<<< 6da89c3e3937c5b6c5c0ecf686563e9dbbee7ab5
 passport.use(new TwitterStrategy(
   {
     consumerKey: TWITTER_CONSUMER_KEY,
@@ -50,3 +53,18 @@ passport.use(new TwitterStrategy(
     done(null, userTwitterInfo);
   }
 ));
+=======
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = require('../../__cutestuff');
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.localhost:3000/auth/facebook/callback",
+  }, (accessToken, refreshToken, profile, cb) => {
+    cb(accessToken);
+  }
+));
+
+>>>>>>> to merge
