8e182e17b3095897a8a7aae4d3db62025a62dc31
Matt Dubie <mbdubie@gmail.com>
2016-08-13 14:48:27 -0700

    commit to merge

--- server/auth/auth.controller.js
+++ server/auth/auth.controller.js
@@ -2,8 +2,7 @@ const Settings = require('../settings/setting.model');
 const jwt = require('jwt-simple');
 const fetch = require('isomorphic-fetch');
 const config = require('../config/config');
-const { LINKEDIN_KEY, LINKEDIN_SECRET } = require('../../__cutestuff');
-const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = require('../../__cutestuff');
+const { LINKEDIN_KEY, LINKEDIN_SECRET, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET } = require('../../__cutestuff');
 
 const saveTwitterTokens = (req, res) => {
   const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
@@ -31,11 +30,9 @@ const saveTwitterTokens = (req, res) => {
   .then(() => {
     res.redirect('/');
   })
-  .catch(err => console.log(err));
 };
 
 const saveLinkedInToken = (req, res) => {
-console.log('saveLinkedInToken');
   const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
   fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${req.query.code}&redirect_uri=http%3A%2F%2F127.0.0.1%3A3000%2Fauth%2Flinkedin%2Fcallback&client_id=${LINKEDIN_KEY}&client_secret=${LINKEDIN_SECRET}`, {
     method: 'POST',
@@ -68,20 +65,19 @@ console.log('saveLinkedInToken');
   })
   .then(() => {
     res.redirect('/');
-  })
-  .catch(err => console.log(err));
+  });
 };
 
 const saveFacebookToken = (req, res) => {
-  console.log('req gettting hit', req);
+  const code = req.query.code;
+  const redirectURI = 'http%3A%2F%2Fwww.localhost%3A3000%2Fauth%2Ffacebook%2Fcallback';
   const { userId } = jwt.decode(req.cookies.jwtStuff, config.secret);
-  console.log('userId', userId);
-  const url = `https://www.facebook.com/dialog/oauth?client_id=${FACEBOOK_APP_ID}&response_type=token&redirect_uri=http%3A%2F%2Fwww.localhost%3A3000%2Fauth%2Ffacebook%2Fcallback`
+  const url = `https://graph.facebook.com/v2.3/oauth/access_token?client_id=${FACEBOOK_APP_ID}&redirect_uri=${redirectURI}&client_secret=${FACEBOOK_APP_SECRET}&code=${code}`;
+
   fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'cors': 'no-cors',
     },
   })
   .then(res => res.json())
@@ -101,14 +97,14 @@ const saveFacebookToken = (req, res) => {
         Settings.create({
           userUserId: userId,
           platform: 'facebook',
-          token: json.access_Token,
+          token: json.access_token,
         });
       }
     });
   })
   .then(() => {
     res.redirect('/');
-  })
+  });
 };
 
 module.exports = {
