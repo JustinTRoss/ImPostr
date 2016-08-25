44349c948cab3348ff1a450b6f46be96f4163538
Matt Dubie <mbdubie@gmail.com>
2016-08-24 19:48:59 -0700

    reformmated platform list entry and platform list removed entry

--- client/components/PostQueueRemovedEntry.js
+++ client/components/PostQueueRemovedEntry.js
@@ -4,17 +4,24 @@ import moment from 'moment';
 const PostQueueRemovedEntry = ({ post, requestRemove, index }) => (
   <div className="PQEContainer">
     <div
-      className="PQEAvatar"
-      style={{"backgroundImage": "url('https://pbs.twimg.com/profile_images/739532954431426562/6_o9x8It_normal.jpg')" }}
+      className="PQELogo"
+      style={{"backgroundImage": `url(../style/${post.platform}icon.png)` } }
     >
     </div>
     <div className="PostQueueEntry">
-      {`${post.message} will NOT be posted anymore on ${post.platform} at ${moment(post.expires).fromNow()}`}
-      <button
-        type="button"
-        className="btn btn-link PQERemoveButton"
-        onClick={() => { requestRemove(post, index); }}
-      >Add Back</button>
+      <div>
+        {post.message}
+      </div>
+      <div className="PostQueueEntryRight">
+        <div>
+          {moment(post.expires).fromNow()}
+        </div>
+        <button
+          type="button"
+          className="btn btn-link PQERemoveButton"
+          onClick={() => { requestRemove(post, index); }}
+        >Add Back</button>
+      </div>
     </div>
   </div>
 );
