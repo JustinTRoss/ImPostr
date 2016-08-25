491d694f7d3758666e1f1e545fb47ea65cd7ae09
Justin Ross <Ross.Justin.T@gmail.com>
2016-08-24 17:32:32 -0700

    add moment to HistoryListItems

--- client/components/HistoryListItem.js
+++ client/components/HistoryListItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import moment from 'moment';
 
 const HistoryListItem = ({ history }) => {
   let { message, platform, updatedAt } = history;
   return (
     <div>
-      {`'${message}' was posted on ${platform} at ${updatedAt}`}
+      {`'${message}' was posted on ${platform} at ${moment(updatedAt).fromNow()}`}
     </div>
   );
 };
